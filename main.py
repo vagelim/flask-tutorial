fd60386e3149bf8df11e67bf85df8ae1f2791148
Evan Mouzakitis <vagelim@users.noreply.github.com>
2018-09-20 12:10:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
 from flask import Flask
+import requests
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    # Read from database:
+    r = requests.get('user:super_secret_pass@remote-resource.com/response.html')
+    return r.text
 
 # Runs on port 5000 by default
 if __name__ == "__main__":
